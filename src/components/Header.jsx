4c6c4698fb91a2cecce2ed34dd7e4c1a7bf64c7e
Anshu Kumar Singh <anshukrsingh11@gmail.com>
2024-06-05 09:42:55 +0530

    Created header

---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <div className="fixed top-0 w-full py-5 text-4xl font-bold text-center shadow-sm bg-gradient-to-br from-slate-50 to-emerald-100">Blog by Ansh</div>;
+}
+
+export default Header;
