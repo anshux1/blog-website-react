96a55f8e3824e24ea26db774086603b674e8153c
Anshu Kumar Singh <anshukrsingh11@gmail.com>
2024-06-05 10:51:10 +0530

    Fixed spinner position to center

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -7,7 +7,10 @@ function Blogs() {
   return <div className="max-w-5xl pt-24 pr-5 mx-auto">
     <div>
     {/* <BlogCard /> */}
-    {loading ? (<span className="loading loading-spinner text-primary"></span>) : 
+    
+    {loading ? (<div className="flex items-center justify-center h-screen">
+      <span className="w-24 loading loading-spinner text-primary"></span>
+      </div>):
     (posts && posts.map((post) => <BlogCard key={post.id} post={post} /> ))  }
     </div>
   </div>;
